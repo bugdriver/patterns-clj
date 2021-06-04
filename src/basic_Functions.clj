d0bfa44db4c6042aeda84c05087680429f4af722
rajat <rajat.kushawaha@thoughtworks.com>
2021-06-04 09:31:24 +0530

    added HOF my-map , my-filter and my-reduce

--- src/basic_Functions.clj
+++ src/basic_Functions.clj
@@ -12,6 +12,32 @@
 (defn reverse-list [list]
   (loop [lt list
          rl '()]
-    (if (zero? (count lt))
+    (if (empty? lt)
       rl
       (recur (rest lt) (conj rl (first lt))))))
+
+
+(defn my-map [func list]
+  (loop [l list
+         nl []]
+    (if (empty? l)
+      nl
+      (recur (rest l) 
+             (conj nl (func (first l)))))))
+
+(defn my-filter [func list]
+  (loop [l list
+         nl []]
+    (if (empty? l)
+      nl
+      (recur (rest l)
+             (if (func (first l))
+               (conj nl (first l))
+               nl)))))
+
+(defn my-reduce [reducer context list]
+  (loop [l list
+         r context]
+    (if (empty? l)
+      r
+      (recur (rest l) (reducer r (first l))))))
--- test/basic_Functions_test.clj
+++ test/basic_Functions_test.clj
@@ -34,3 +34,30 @@
     )
   )
     
+(deftest ^:implemented my-map-test
+  (testing "Returns the new list with elements mapped with given function"
+    (are [x y z] (= x (b/my-map y z))
+      [2 3 4] inc [1 2 3]
+      [1 2 3] identity [1 2 3]
+      [1 2 nil]  #{1 2}   [1 2 3]
+      [1 2 3] :a [{:a 1 :b 2} {:a 2 :c 3} {:d 4 :a 3}]
+      ))
+  )
+
+(deftest ^:implemented my-filter-test
+  (testing "Returns the new filtered list with given predicate"
+    (are [x y z] (= x (b/my-filter y z))
+      [2 4] even? [1 2 3 4]
+      [1 3] odd? [1 2 3]
+      [1 2]  #{1 2}   [1 2 3]
+      [{:a 2}] :a [{:b 2} {:a 2} {:d 4}]
+      ))
+  )
+
+(deftest ^:implemented my-reduce-test
+  (testing "Returns the reduced result with given reducer and context"
+    (are [p q r s] (= p (b/my-reduce q r s))
+      10 + 0 [1 2 3 4]
+      6 * 1 [1 2 3]
+      '(3 2 1) conj '() [1 2 3]
+  )))
